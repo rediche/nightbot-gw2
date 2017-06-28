8ee8c46c38e86b8ddcc611e0eca43bc61509b2e6
Marcus Geil <mg@writereader.com>
2017-06-28 22:42:41 +0200

    Installed lumen

---
+++ resources/views/frontpage.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>GW2 for Nightbot</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--<link href="css/style.css" rel="stylesheet">-->
+  </head>
+  <body>
+    <h1>GW2 for Nightbot</h1>
+  </body>
+</html>
