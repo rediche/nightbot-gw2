127a434694fe45ae095da5e1731d3c65cbeed1ec
Marcus Geil <mg@writereader.com>
2017-07-01 13:54:25 +0200

    Started work on frontend

--- resources/views/frontpage.blade.php
+++ resources/views/frontpage.blade.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
 
-    <title>nightbot-gw2</title>
-    <meta name="description" content="nightbot-gw2 description">
+    <title>GW2 for Nightbot</title>
+    <meta name="description" content="Guild Wars 2 custom commands for Nightbot.">
 
     <!-- See https://goo.gl/OOhYW5 -->
     <link rel="manifest" href="/manifest.json">
@@ -13,6 +13,13 @@
     <script src="/bower_components/webcomponentsjs/webcomponents-loader.js"></script>
 
     <link rel="import" href="/src/nightbot-gw2-app/nightbot-gw2-app.html">
+
+    <style>
+      body {
+        padding: 0;
+        margin: 0;
+      }
+    </style>
   </head>
   <body>
     <nightbot-gw2-app></nightbot-gw2-app>
